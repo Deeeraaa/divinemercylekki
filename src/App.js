b2d779906eecb9abc7285319203bdca649942909
Dera <stephenie7533@gmail.com>
2022-06-22 09:36:40 +0100

    Primary commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import './App.css';
 
 // import Article from './components/article/Article'
@@ -12,21 +14,43 @@ import { Footer, Blog, Header, WhatGPT3, Possibilities } from './containers'
 
 
 function App() { 
+  const [loading, setLoading] = useState(false);
+  
+  
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    },3500)
+  }, [])
+
   return (
     <div className="App"> 
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Article/>
-      <Brand />
-      <WhatGPT3 />
-      <Feature />
-      <Program />
-      <Possibilities />
-      <Cta />
-      <Blog />
-      <Footer />
+      {loading ?
+        <div className="loader">
+            <ClimbingBoxLoader classame='loader' color={'#366CD7'} loading={loading} size={30} />
+        </div>
+        
+        : 
+        <div>
+         <div className="gradient__bg">
+            <Navbar />
+            <Header />
+          </div>
+          <Article/>
+          <Brand />
+          <WhatGPT3 />
+          <Feature />
+          <Program />
+          <Possibilities />
+          <Cta />
+          <Blog />
+              <Footer />
+            </div>    
+      
+        
+        }
+      
     </div>
   );
 }
